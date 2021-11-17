ea951fd5b857d017c1de426c86c9b9c916f78dee
RahmaBoussaffa <77215103+RahmaBoussaffa@users.noreply.github.com>
2021-11-18 00:31:11 +0100

    Add files via upload

---
+++ photos/src/main/java/com/rahma/photos/entities/Photographe.java
@@ -0,0 +1,27 @@
+package com.rahma.photos.entities;
+import java.util.List;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Photographe {
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+private Long idPhot;
+private String nomPhot;
+private String descriptionPhot;
+@JsonIgnore
+@OneToMany(mappedBy = "photographe")
+private List<Photo> photos;
+}
